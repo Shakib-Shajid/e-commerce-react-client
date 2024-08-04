1f0b8e45203b707976f128bb1c1408daf90bd6a0
Shakib-Shajid <shakibalshajid1682@gmail.com>
2024-08-04 22:47:44 +0600

    register function added

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,7 +1,7 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { app } from "../firebase/firebase.config";
 
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
 export const AuthContext = createContext(null);
 
@@ -16,14 +16,43 @@ const AuthProvider = ({ children }) => {
     const [loading, setLoading] = useState(true);
 
 
-    // 
+    // auth change
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            setLoading(false);
+        })
+        return () => {
+            return unSubscribe();
+        }
+    }, [])
+
+    // login
+    const loginUser = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    // create user
+    const signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const logOut = () => {
+        setLoading(true)
+        return signOut(auth);
+    }
+
 
 
 
     const authInfo = {
         user,
         loading,
-
+        loginUser,
+        signIn,
+        logOut
     }
 
 
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -2,39 +2,51 @@ import { Link } from "react-router-dom";
 
 const Register = () => {
 
+    const handleSignIn = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        const cpassword = form.cpassword.value;
+        const ruser = {name, email, password, cpassword};
+        console.log(ruser);
+    }
+
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="card w-full md:w-2/4">
                 <h3 className="text-xl md:text-3xl font-bold text-center mt-5">Registration Form</h3>
-                <form className="card-body">
+                <form className="card-body" onSubmit={handleSignIn}>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Full Name</span>
                         </label>
-                        <input type="text" placeholder="Full Name" className="input input-bordered" required />
+                        <input type="text" name="name" placeholder="Full Name" className="input input-bordered" required />
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Email</span>
                         </label>
-                        <input type="email" placeholder="Email" className="input input-bordered" required />
+                        <input type="email" name="email" placeholder="Email" className="input input-bordered" required />
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Password</span>
                         </label>
-                        <input type="password" placeholder="Password" className="input input-bordered" required />
+                        <input type="password" name="password" placeholder="Password" className="input input-bordered" required />
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Confirm Password</span>
                         </label>
-                        <input type="password" placeholder="Confirm Password" className="input input-bordered" required />
+                        <input type="password" name="cpassword" placeholder="Confirm Password" className="input input-bordered" required />
                     </div>
-                
+
 
                     <div className="form-control mt-6">
-                        <button className="btn btn-primary">Sign Up</button>
+                        {/* <button className="btn btn-primary">Sign Up</button> */}
+                        <input type="submit" value="Sign Up" className="btn btn-primary" />
                     </div>
                     <small>Already have an account? <span className="font-bold"><Link to="/login" className="text-blue-700 text-bold">Click Here</Link></span></small>
                 </form>
