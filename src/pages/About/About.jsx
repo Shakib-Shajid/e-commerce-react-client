b1fbf3815d33fdd79efcf844714dad91f2cc8352
Shakib-Shajid <shakibalshajid1682@gmail.com>
2024-08-04 13:11:15 +0600

    about page added

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,22 @@
+
+const About = () => {
+    return (
+            <div
+                className="hero min-h-screen"
+                style={{
+                    backgroundImage: "url(https://i.ibb.co/LC4Xpxb/bike.jpg)",
+                }}>
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-neutral-content text-center">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold">About Us</h1>
+                        <p className="mb-5">
+                            Trust and Good Products is our main target to serve. Your trust is our main success.
+                        </p>
+                    </div>
+                </div>
+            </div>
+    );
+};
+
+export default About;
